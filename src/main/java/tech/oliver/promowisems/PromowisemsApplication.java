e935378b7032f69e85f016ce14a16a34273da49d
OliverBR1 <Br.0liveira@outlook.com>
2026-09-11 11:18:26 -0300

    new features

--- src/main/java/tech/oliver/promowisems/PromowisemsApplication.java
+++ src/main/java/tech/oliver/promowisems/PromowisemsApplication.java
@@ -2,8 +2,10 @@ package tech.oliver.promowisems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PromowisemsApplication {
 
 	public static void main(String[] args) {
--- src/test/java/tech/oliver/promowisems/TestPromowisemsApplication.java
+++ src/test/java/tech/oliver/promowisems/TestPromowisemsApplication.java
@@ -2,10 +2,16 @@ package tech.oliver.promowisems;
 
 import org.springframework.boot.SpringApplication;
 
+import static tech.oliver.promowisems.ContainerConfig.getProperties;
+import static tech.oliver.promowisems.ContainerConfig.wireMockContainer;
+
 public class TestPromowisemsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(PromowisemsApplication::main).with(TestcontainersConfiguration.class).run(args);
+		wireMockContainer.start();
+		getProperties().forEach(System::setProperty);
+		SpringApplication.from(PromowisemsApplication::main)
+				.with(ServiceConnectionConfig.class).run(args);
 	}
 
 }
