47f81714e312e76b7811d421351b0e20154bb959
OliverBR1 <Br.0liveira@outlook.com>
2026-09-11 11:21:28 -0300

    created interfaces

---
+++ src/main/java/tech/oliver/promowisems/client/TelemetryClient.java
@@ -0,0 +1,14 @@
+package tech.oliver.promowisems.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import tech.oliver.promowisems.client.dto.TelemetryRequest;
+
+@FeignClient(name = "telemetryClient", url = "${telemetry.api.url}")
+public interface TelemetryClient {
+
+    @PostMapping("/v1/events")
+    void sendEvent(@RequestHeader("x-api-key") String apiKey, TelemetryRequest request);
+
+}
